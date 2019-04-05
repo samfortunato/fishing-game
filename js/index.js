18ca1400f215e8abcbb66981d1713fef34cfce9f
Samuel Fortunato <samfortunato@gmail.com>
2019-04-04 20:55:17 -0400

    Add common, uncommon, and rare fish

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// game
+const commonFish = [
+  'Small Bass',
+  'Freshwater Goby',
+  'Carp',
+  'Sea Bass'
+];
+
+const uncommonFish = [
+  'Bass',
+  'Dace',
+  'Crucian Carp'
+];
+
+const rareFish = [
+  'Killifish',
+  'Pagoda',
+  'Koi'
+];
