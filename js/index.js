b24866213d0578fe79668b2f39419951f87f648b
Samuel Fortunato <samfortunato@gmail.com>
2019-04-04 21:06:12 -0400

    Make function to get random fish where fish have rarity values

--- js/index.js
+++ js/index.js
@@ -16,3 +16,25 @@ const rareFish = [
   'Pagoda',
   'Koi'
 ];
+
+const randomNumFromOneToN = (n) => {
+  return Math.floor((Math.random() * n)) + 1;
+};
+
+const randomElementFromArray = (array) => {
+  const randomIndex = randomNumFromOneToN(array.length) - 1;
+  return array[randomIndex];
+};
+
+const goFish = () => {
+  const randomNum = randomNumFromOneToN(100);
+  console.log(randomNum);
+
+  if (randomNum < 60) {
+    return randomElementFromArray(commonFish);
+  } else if (randomNum >= 60 && randomNum < 85) {
+    return randomElementFromArray(uncommonFish);
+  } else if (randomNum >= 85) {
+    return randomElementFromArray(rareFish);
+  }
+};
